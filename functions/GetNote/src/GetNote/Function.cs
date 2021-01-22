c98ddd66ac3c6ec2095b5053c9a06026ee77bc77
Henrik Fricke <henrikfricke@web.de>
2021-01-22 20:18:50 +0100

    feat: fetch note from dynamodb

--- functions/GetNote/src/GetNote/Function.cs
+++ functions/GetNote/src/GetNote/Function.cs
@@ -1,12 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 
 using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -22,14 +21,40 @@ namespace GetNote
 
     public class Function
     {
-        public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest apigProxyEvent, ILambdaContext context)
+        private static AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+
+        public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest apigProxyEvent, ILambdaContext context)
         {
             var id = apigProxyEvent.PathParameters["id"];
+            var tableName = System.Environment.GetEnvironmentVariable("TABLE_NAME");
 
-            var note = new Note();
-            note.id = id;
-            note.title = "Hello World";
-            note.content = "Culpa qui veniam sint minim ut aliquip officia veniam voluptate.";
+            var result = await client.GetItemAsync(new GetItemRequest
+            {
+                TableName = tableName,
+                Key = new Dictionary<string, AttributeValue>()
+                {
+                    { "id", new AttributeValue {
+                        S = id
+                    } }
+                },
+                ProjectionExpression = "id, title, content",
+                ConsistentRead = true
+            });
+
+            if (result.Item.Count == 0)
+            {
+                return new APIGatewayProxyResponse
+                {
+                    StatusCode = 400,
+                };
+            }
+
+            var note = new Note
+            {
+                id = result.Item["id"].S,
+                title = result.Item["title"].S,
+                content = result.Item["content"].S
+            };
 
             return new APIGatewayProxyResponse
             {
