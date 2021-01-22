a014b76639be3c57ddb817a386dd1b29b8e46c3d
Henrik Fricke <henrikfricke@web.de>
2021-01-22 09:56:46 +0100

    Initial commit

---
+++ src/LambdaDotnetExample/LambdaDotnetExampleStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace LambdaDotnetExample
+{
+    public class LambdaDotnetExampleStack : Stack
+    {
+        internal LambdaDotnetExampleStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
