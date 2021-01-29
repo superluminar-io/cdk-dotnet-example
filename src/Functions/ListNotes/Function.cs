315c967fd70686c2b727722b9f02a012b1525379
Henrik Fricke <henrikfricke@web.de>
2021-01-29 16:57:11 +0100

    test: ListNotes function

--- src/Functions/ListNotes/Function.cs
+++ src/Functions/ListNotes/Function.cs
@@ -1,30 +1,45 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
-using Amazon.Lambda.Core;
-using Amazon.Lambda.APIGatewayEvents;
 using Amazon.DynamoDBv2;
-using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-namespace Function
+namespace ListNotes
 {
+    public class Note
+    {
+        public string id { get; set; }
+        public string title { get; set; }
+        public string content { get; set; }
+    }
+
     public class Function
     {
-        private static AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+        private AmazonDynamoDBClient client = new AmazonDynamoDBClient();
 
-        public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest apigProxyEvent, ILambdaContext context)
+        public Function() { }
+
+        public Function(AmazonDynamoDBClient c) => client = c;
+
+
+        public async Task<APIGatewayProxyResponse> FunctionHandler()
         {
             var tableName = System.Environment.GetEnvironmentVariable("TABLE_NAME");
-            var notesTable = Table.LoadTable(client, tableName);
-            var search = notesTable.Scan(new ScanFilter());
-            var results = await search.GetNextSetAsync();
+            var response = await client.ScanAsync(tableName, new List<string> { "id", "title", "content" });
+            var notes = response.Items.ConvertAll<Note>((values) => new Note
+            {
+                id = values["id"].S,
+                title = values["title"].S,
+                content = values["content"].S
+            });
 
             return new APIGatewayProxyResponse
             {
-                Body = results.ToJson(),
+                Body = JsonSerializer.Serialize(notes),
                 Headers = new Dictionary<string, string> { {
                     "Content-Type", "application/json"
                 } },
---
+++ tests/Functions.Tests/ListNotes.Tests/FunctionTest.cs
@@ -0,0 +1,48 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using Moq;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading;
+using Xunit;
+
+namespace ListNotes.Tests
+{
+    public class FunctionTest
+    {
+        [Fact]
+        public async void TestListNotesFunction()
+        {
+            var tableName = "NOTES";
+            System.Environment.SetEnvironmentVariable("TABLE_NAME", tableName);
+
+            var attributesToGet = new List<string> { "id", "title", "content" };
+            var item1 = new Dictionary<string, AttributeValue> {
+                { "id", new AttributeValue { S = "abc"} },
+                { "title", new AttributeValue { S = "hello world"} },
+                { "content", new AttributeValue { S = "Et aliqua adipisicing incididunt eiusmod."} },
+            };
+            var items = new List<Dictionary<string, AttributeValue>> { item1 };
+            var note = new Note
+            {
+                id = "abc",
+                title = "hello world",
+                content = "Et aliqua adipisicing incididunt eiusmod."
+            };
+            var notes = new List<Note> { note };
+
+            var mock = new Mock<AmazonDynamoDBClient>();
+            mock.Setup(library => library.ScanAsync(tableName, attributesToGet, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new ScanResponse
+                {
+                    Items = items
+                });
+
+            var function = new Function(mock.Object);
+            var response = await function.FunctionHandler();
+
+            Assert.Equal(200, response.StatusCode);
+            Assert.Equal(JsonSerializer.Serialize(notes), response.Body);
+        }
+    }
+}
